ff0c6e695509f88cccf69713b79ed93c870a2ce4
Radwa Mabchour <radwa_mabchour@um5.ac.ma>
2026-02-08 12:48:53 +0000

    Update DepartmentControllerTest.java

--- backend/src/test/java/com/example/backend/DepartmentControllerTest.java
+++ backend/src/test/java/com/example/backend/DepartmentControllerTest.java
@@ -4,6 +4,7 @@ import com.example.backend.controller.DepartmentController;
 import com.example.backend.service.DepartmentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,6 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(DepartmentController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class DepartmentControllerTest {
 
     @Autowired
@@ -21,8 +23,9 @@ class DepartmentControllerTest {
     private DepartmentService departmentService;
 
     @Test
-    void getAllDepartments_shouldReturnOkOrEmpty() throws Exception {
+    void getDepartments_contextLoads() throws Exception {
         mockMvc.perform(get("/api/departments"))
                .andExpect(status().isOk());
     }
 }
+
