ae13fbfd1f8e11dfba633daa5d8c52762a92a61e
Radwa Mabchour <radwa_mabchour@um5.ac.ma>
2026-02-08 12:22:23 +0000

    Update DepartmentControllerTest.java

--- backend/src/test/java/com/example/backend/DepartmentControllerTest.java
+++ backend/src/test/java/com/example/backend/DepartmentControllerTest.java
@@ -5,6 +5,7 @@ import com.example.backend.service.DepartmentService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(DepartmentController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class DepartmentControllerTest {
 
     @Autowired
