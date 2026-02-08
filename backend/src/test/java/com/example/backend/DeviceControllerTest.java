ad26be538ac0a768971d1b9cc410e32186710e60
Radwa Mabchour <radwa_mabchour@um5.ac.ma>
2026-02-08 12:22:53 +0000

    Update DeviceControllerTest.java

--- backend/src/test/java/com/example/backend/DeviceControllerTest.java
+++ backend/src/test/java/com/example/backend/DeviceControllerTest.java
@@ -5,6 +5,7 @@ import com.example.backend.service.DeviceService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(DeviceController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class DeviceControllerTest {
 
     @Autowired
@@ -33,3 +35,4 @@ class DeviceControllerTest {
                .andExpect(status().isOk());
     }
 }
+
