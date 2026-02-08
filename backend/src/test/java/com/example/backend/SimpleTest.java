40e4c9aa2b58176be3d2a88f790c313c793fae91
Radwa Mabchour <radwa_mabchour@um5.ac.ma>
2026-02-08 12:55:39 +0000

    Update and rename AuthControllerTest.java to SimpleTest.java

---
+++ backend/src/test/java/com/example/backend/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.example.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleTest {
+
+    @Test
+    void simple_math_test() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
+
