25768ac64ac65eaa47efb1d7110719402f036b45
Radwa Mabchour <radwa_mabchour@um5.ac.ma>
2026-02-08 12:21:27 +0000

    Update AuthControllerTest.java
    desactiver securite

--- backend/src/test/java/com/example/backend/AuthControllerTest.java
+++ backend/src/test/java/com/example/backend/AuthControllerTest.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
 
     @Autowired
@@ -50,3 +52,4 @@ class AuthControllerTest {
                 .andExpect(status().isOk());
     }
 }
+
